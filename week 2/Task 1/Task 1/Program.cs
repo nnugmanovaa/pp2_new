a8c72ffce3828019e7088da40110aafa57ba936e
nugmanova2001 <nugmanova_2001@mail.ru>
2019-02-09 00:19:51 +0600

    comment

---
+++ week 2/Task 1/Task 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read file;
+            StreamReader sr = new StreamReader(@"C:\Users\user\Desktop\pp2_!\week 2\Task 1\Input.txt");
+            string s;
+            //read each line in the file
+            while ((s = sr.ReadLine()) != null)
+            {
+                //reload to char array
+                char[] chararray = s.ToCharArray();
+                //reverse char array 
+                Array.Reverse(chararray);
+                //reload it to string 
+                string check = new string(chararray);
+                if (check == s)// if reverse string and origin are equal , it is a palindrome 
+                    Console.WriteLine("Yes");
+                else Console.WriteLine("No");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week 2/Task 2/Task 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"D:\input.txt");
+            string s = sr.ReadLine();
+            StreamWriter sw = new StreamWriter(@"D:\output.txt");
+            string[] arr = s.Split();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int current = int.Parse(arr[i]);
+                bool test = true;
+                for (int j = 2; j <= Math.Sqrt(current); j++)
+                {
+                    if (current % j == 0)
+                    {
+                        test = false;
+                        break;
+                    }
+                }
+                if (test && current != 1)
+                {
+                    sw.Write(current + " ");
+                }
+            }
+            sr.Close();
+            sw.Close();
+            Console.ReadKey();
+        }
+    }
+}
