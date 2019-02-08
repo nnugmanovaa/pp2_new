9f24b17d7f9ae229575881e6c5977cc19c05e246
nugmanova2001 <nugmanova_2001@mail.ru>
2019-02-09 00:25:51 +0600

    comment

---
+++ week 2/Task 3/Task 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+namespace Task_3
+{
+    class Program
+    {
+        public static void showspaces(int level)
+        {
+            for (int i = 0; i < level; i++)
+            {
+                Console.Write(" ");
+            }
+        }
+        public static void showDirectory(DirectoryInfo d, int level)
+        {
+            FileInfo[] fi = d.GetFiles();
+            DirectoryInfo[] di = d.GetDirectories();
+
+            foreach (FileInfo f in fi)
+            {
+                showspaces(level);
+                Console.WriteLine(f.Name);
+            }
+            foreach (DirectoryInfo directory in di)
+            {
+                showspaces(level);
+                Console.WriteLine(directory.Name);
+                showDirectory(directory, level + 1);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\user\Desktop");
+            showDirectory(d, 0);
+            Console.ReadKey();
+        }
+    }
+}
+
