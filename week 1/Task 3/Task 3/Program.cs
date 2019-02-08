de64dc40e7f42f6a19b3375ba189321ad24f127d
nugmanova2001 <nugmanova_2001@mail.ru>
2019-02-09 00:01:18 +0600

    comment

---
+++ week 1/Task 3/Task 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); //read size of array
+            int[] a = new int[n]; //create array of size n
+            string[] ss = Console.ReadLine().Split(); //split sequence
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(ss[i]); //from string to int each element 
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} {0} ", a[i]); //double output each element of array
+            }
+            Console.ReadLine();
+        }
+    }
+}
